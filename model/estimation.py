e6d3dffaf3d2b201d81dcebc12e90e51ac19b366
Alexander Batashev <alexbatashev@outlook.com>
2023-09-10 21:42:48 +0300

    Attempt at new methods

--- model/estimation.py
+++ model/estimation.py
@@ -1,4 +1,4 @@
-from torch.nn import Module, Linear, LSTM
+from torch.nn import Module, Linear, LSTM, Dropout2d, BatchNorm1d, GRU
 import torch.nn.functional as F
 from torch_geometric.nn import GraphConv, GCNConv, GraphNorm, BatchNorm
 from torch_geometric.utils import to_dense_batch
@@ -12,7 +12,6 @@ class GNNEstimation(Module):
         self.hidden_dim = hidden_dim
         self.batch_size = batch_size
 
-        # self.embedding = TemporalEncoding(hidden_dim)
         self.embedding = torch.nn.Embedding(21000, input_dim)
         self.norm1 = GraphNorm(input_dim)
 
@@ -25,41 +24,35 @@ class GNNEstimation(Module):
 
         self.norm2 = BatchNorm(hidden_dim)
 
-        # self.fc1 = Linear(hidden_dim, hidden_dim)
-        self.lstm = LSTM(hidden_dim, hidden_dim, batch_first=True)
+        self.bidirectional = True
 
-        self.fc2 = Linear(hidden_dim, output_dim)
+        self.lstm = LSTM(hidden_dim, hidden_dim, batch_first=True, bidirectional=self.bidirectional)
 
-        self.hidden = self.init_hidden()
-
-    def init_hidden(self):
-        return torch.zeros([1, self.batch_size, self.hidden_dim]).cuda().detach(), torch.zeros([1, self.batch_size, self.hidden_dim]).cuda().detach()
+        self.concat = Linear(hidden_dim * 2, hidden_dim)
+        self.dropout = Dropout2d(p=0.3)
+        self.fc = Linear(hidden_dim, output_dim)
 
     def forward(self, data):
         x = data.x
         edge_index = data.edge_index
         batch = data.batch
 
-        # x = self.norm1(x)
-
         x = self.embedding(x)
 
         x = self.conv(x, edge_index)
-        x = F.relu(x)
+        # x = F.relu(x)
 
         x = self.norm2(x)
 
         nodes, mask = to_dense_batch(x, batch)
 
-        # nodes = self.fc1(nodes)
-        # nodes = F.relu(nodes)
-
-        x, hidden = self.lstm(nodes[:, 1:, :], self.hidden)
-        h0, h1 = hidden
-        self.hidden = (h0.detach(), h1.detach())
+        x, _ = self.lstm(nodes[:, 1:, :])
         x = x[:, -1, :]
         x = x.reshape(self.batch_size, -1)
-        x = self.fc2(x)
+        if self.bidirectional:
+            x = self.concat(x)
+            x = F.relu(x)
+        x = self.fc(x)
         x = F.relu(x)
         return x.squeeze()
 
@@ -71,35 +64,43 @@ class LSTMEstimation(Module):
         self.hidden_dim = hidden_dim
         self.batch_size = batch_size
 
-        self.embedding = torch.nn.Embedding(21000, input_dim)
+        self.embedding = torch.nn.Embedding(21000, input_dim, scale_grad_by_freq=True)
 
-        # self.norm = BatchNorm(input_dim)
+        self.norm1 = BatchNorm1d(input_dim)
 
-        self.lstm = LSTM(input_dim, hidden_dim, batch_first=True)
+        self.bidirectional = True
+
+        self.lstm = LSTM(input_dim, hidden_dim, batch_first=True, bidirectional=self.bidirectional)
+
+        self.concat = Linear(2 * hidden_dim, hidden_dim)
+
+        self.norm2 = BatchNorm1d(hidden_dim)
 
         self.fc = Linear(hidden_dim, output_dim)
-
-        self.hidden = self.init_hidden()
-
-    def init_hidden(self):
-        return torch.rand([1, self.batch_size, self.hidden_dim]).cuda().detach(), torch.rand([1, self.batch_size, self.hidden_dim]).cuda().detach()
 
     def forward(self, data):
         x = data.x
         batch = data.batch
 
-        # x = self.norm(x)
-
         x = self.embedding(x)
 
         nodes, mask = to_dense_batch(x, batch)
 
-        x, hidden = self.lstm(nodes, self.hidden)
-        h0, h1 = hidden
-        self.hidden = (h0.detach(), h1.detach())
-        x = x[:, -1, :]
+        # FIXME this is only allowed when we have has_virtual_root=True
+        nodes = nodes[:, 1:, :]
+
+        #nodes = torch.transpose(nodes, 1, 2)
+        #nodes = self.norm1(nodes)
+        #nodes = torch.transpose(nodes, 1, 2)
+
+        x, _ = self.lstm(nodes)
+        x = x[:, 0, :]
         x = x.reshape(self.batch_size, -1)
-        x = F.relu(x)
+
+        if self.bidirectional:
+            x = self.concat(x)
+            x = F.relu(x)
+
         x = self.fc(x)
         x = F.relu(x)
         return x.squeeze()
--- model/utils.py
+++ model/utils.py
@@ -1,144 +1,6 @@
 import io
-import math
-import os
-import json
-import torch
-import yaml
-from torch_geometric.data import Data, Dataset
-from torch import nn
-from torch_geometric.utils import add_self_loops
-import model.mc_dataset_pb2
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-class BasicBlockDataset(Dataset):
-    def __init__(self, dataset_path, embedding_size=0, max_cache_miss=0, min_nodes=3, min_cycles=1., max_context_switches=0, dtype=torch.float32):
-        super().__init__(None, None, None)
-        with open(dataset_path, 'rb') as f:
-            dataset = model.mc_dataset_pb2.MCDataset()
-            dataset.ParseFromString(f.read())
-
-            #self.num_opcodes = dataset.data[0].graph.num_opcodes
-            self.num_opcodes = 16
-            self.has_virtual_root = dataset.data[0].graph.has_virtual_root
-
-            self.embeddings = []
-            self.raw = []
-            self.measurements = []
-            for d in dataset.data:
-                if getattr(d.metrics, 'measured_num_runs', 0) == 0:
-                    continue
-                if d.metrics.measured_cycles < 0:
-                    continue
-                if d.metrics.measured_cycles / getattr(d.metrics, 'measured_num_runs') > 50:
-                    continue
-                if d.metrics.workload_cache_misses - d.metrics.noise_cache_misses != 0:
-                    continue
-                if d.metrics.workload_context_switches - d.metrics.noise_context_switches != 0:
-                    continue
-                # if d.metrics.measured_cycles / 1000 < min_cycles:
-                #     continue
-                # if d.metrics.total_cache_misses > max_cache_miss:
-                #     continue
-                # if d.metrics.total_context_switches > max_context_switches:
-                #     continue
-                # if len(d.graph.nodes) < min_nodes:
-                #     continue
-                nodes = []
-                for n in d.graph.nodes:
-                    if n.is_virtual_root:
-                        nodes.append(torch.zeros(16, dtype=dtype))
-                    else:
-                        binstr = "{0:b}".format(n.opcode)
-                        binary = []
-                        for c in reversed(binstr):
-                            binary.append(float(int(c)))
-                        binary.extend([0] * (16 - len(binstr)))
-                        nodes.append(torch.tensor(binary))
-                    # ohtype = dtype
-                    # if embedding_size > 0:
-                    #     ohtype = torch.int
-                    # one_hot = torch.zeros([d.graph.num_opcodes], dtype=ohtype)
-                    # if not n.is_virtual_root:
-                    #     one_hot[n.onehot] = 1
-                    # if embedding_size == 0:
-                    #     nodes.append(one_hot)
-                    # else:
-                    #     nodes.append(embed(one_hot))
-                nodes = torch.stack(nodes)
-
-                edges = []
-                edge_attrs = []
-                for e in d.graph.edges:
-                    edge_from = getattr(e, 'from', 0)
-                    edge_to = getattr(e, 'to', 0)
-                    edges.append(torch.tensor([edge_from, edge_to]))
-                    edges.append(torch.tensor([edge_to, edge_from]))
-                    edge_attrs.append(torch.tensor(1 if getattr(e, 'is_data', False) else 0))
-                    edge_attrs.append(torch.tensor(1 if getattr(e, 'is_data', False) else 0))
-                if len(edges) == 0:
-                    continue
-                edges = torch.stack(edges).t().contiguous()
-                edge_attrs = torch.stack(edge_attrs)
-
-                self.embeddings.append(Data(x=nodes, edge_index=edges, edge_attr=edge_attrs, y=d.metrics.measured_cycles / getattr(d.metrics, 'measured_num_runs', 1000)))
-
-                #self.measurements.append(d.metrics.measured_cycles / getattr(d.metrics, 'measured_num_runs', 1000))
-
-                raw = {"source": str(d.graph.source),
-                       #"edges": d.graph.edges,
-                       #"nodes": d.graph.nodes
-                       }
-                self.raw.append(raw)
-
-    def len(self):
-        return len(self.embeddings)
-
-    def get(self, index):
-        x = self.embeddings[index]
-        # y = self.measurements[index]
-        z = self.raw[index]
-
-        return x, z
-
-
-def unique_rows(tensor):
-    tensor_np = tensor.cpu().numpy()
-    unique_np = np.unique(tensor_np, axis=0)
-    return torch.tensor(unique_np, device=tensor.device, dtype=tensor.dtype)
-
-
-def get_all_nodes(dataset_path, dtype=torch.float32):
-    with open(dataset_path, 'rb') as f:
-        dataset = model.mc_dataset_pb2.MCDataset()
-        dataset.ParseFromString(f.read())
-
-        num_opcodes = dataset.data[0].graph.num_opcodes
-
-        nodes = []
-
-        for i in range(num_opcodes):
-            onehot = torch.zeros(num_opcodes, dtype=dtype)
-            onehot[i] = 1
-            nodes.append(onehot)
-
-        return torch.stack(nodes)
-
-def save_checkpoint(epoch, model, optimizer, checkpoint_dir):
-    checkpoint_path = os.path.join(checkpoint_dir, f"checkpoint_{epoch}.pt")
-    torch.save({
-        "epoch": epoch,
-        "model_state_dict": model.state_dict(),
-        "optimizer_state_dict": optimizer.state_dict()
-    }, checkpoint_path)
-
-
-def load_checkpoint(checkpoint_path, model, optimizer):
-    checkpoint = torch.load(checkpoint_path)
-    model.load_state_dict(checkpoint["model_state_dict"])
-    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-    return checkpoint["epoch"]
 
 
 def estimate_cycles(port_pressure_sequence):
@@ -174,15 +36,21 @@ def print_port_pressure_table(predicted_port_pressures, instructions):
         print(row_string)
 
 
-def plot_hist(measurements):
+def plot_lift_chart(data):
+    values = np.asarray(data)
+    values = np.transpose(values)
+    values = sorted(values, key=lambda d: d[0])
+    values = np.transpose(values)
+
+    num_samples = len(values[0, :])
+
     plt.figure()
-    plt.plot(60, 60)
-    plt.title("Prediction distribution")
-    max_val = max(np.max(measurements[0]), np.max(measurements[1]))
-    max_val = math.ceil(max_val)
-    plt.hist2d(measurements[0], measurements[1], bins=(np.arange(0, max_val, 3), np.arange(0, max_val, 3)))
-    plt.xlabel("Measured")
-    plt.ylabel("Predicted")
+    items = np.arange(0, num_samples)
+    plt.plot(items, values[1, :], 'b', items, values[0, :], 'r')
+    plt.xlim(0, num_samples)
+    plt.xlabel('Item')
+    plt.ylabel('measured vs predicted')
+    plt.grid(True)
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
