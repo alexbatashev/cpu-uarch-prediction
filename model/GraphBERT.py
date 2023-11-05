d4e52c895b927f624ccdb3b407fde0ee3e2fd0b2
Alexander Batashev <alexbatashev@outlook.com>
2023-11-05 16:21:10 +0300

    save progress

--- model/GraphBERT.py
+++ model/GraphBERT.py
@@ -1,12 +1,25 @@
 import lightning.pytorch as pl
 import torch.nn as nn
 import torch_geometric.nn as gnn
-from torch_geometric.utils import to_dense_batch, to_dense_adj, one_hot
+from torch_geometric.utils import to_dense_batch, to_dense_adj, scatter
 import torch.nn.functional as F
 import torchmetrics
-from torch.optim import Adam, lr_scheduler
+from torch.optim import Adam
 from torch.nn import Module
+from torch.optim import lr_scheduler
 import torch
+
+
+def to_dense_adj_batch(edge_index, batch, num_nodes=0):
+    batch_size = int(batch.max()) + 1
+    adj = torch.zeros((batch_size, num_nodes, num_nodes), dtype=torch.int64, device=edge_index.device)
+
+    one = batch.new_ones(batch.size(0))
+    num_nodes = scatter(one, batch, dim=0, dim_size=batch_size, reduce='sum')
+
+    print(num_nodes)
+
+    return adj
 
 
 class MCEmbedding(Module):
@@ -22,147 +35,248 @@ class MCEmbedding(Module):
 
         output = self.embedding(input_tensor) + pos_tensor
 
-        return self.norm(output)
+        return self.norm(output), pos_tensor
 
 
-class MCGraphAttention(Module):
-    def __init__(self, emb_size, out_size, num_heads):
+class MCAttentionHead(nn.Module):
+    def __init__(self, in_size, heads=4):
         super().__init__()
 
-        self.attention = gnn.DenseGATConv(emb_size, out_size, heads=num_heads)
-        self.linear = nn.Linear(out_size * num_heads, emb_size)
-        self.norm = nn.LayerNorm(emb_size)
+        self.num_heads = heads
 
-    def forward(self, input_tensor, edge_index):
-        dense_scores = self.attention(input_tensor, edge_index)
+        self.key = nn.Linear(in_size, in_size, bias=False)
+        self.query = nn.Linear(in_size, in_size, bias=False)
+        self.value = nn.Linear(in_size, in_size, bias=False)
 
-        dense_scores = self.linear(dense_scores)
+        self.proj = nn.Linear(in_size, in_size, bias=False)
 
-        return self.norm(dense_scores)
+    def forward(self, nodes, edge_index, mask=None):
+        B, T, C = nodes.shape
+
+        mask = mask.view(B, T, 1)
+        mask = torch.broadcast_to(mask, (B, T, C))
+
+        nodes = nodes.masked_fill(mask == False, float(0))
+        # for b in range(B):
+        #     for idx in range(mask.shape[1]):
+        #         if mask[b, idx] is False:
+        #             nodes[b, idx, :] = 0
+
+        k = self.key(nodes).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)
+        q = self.query(nodes).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)
+
+        weight = torch.matmul(q, k.transpose(-2, -1)) * self.num_heads ** -0.5
+
+        scale = 3 * edge_index + torch.ones(edge_index.shape, device=edge_index.device)
+
+        scale = torch.cat([torch.cat([scale[x] for _ in range(self.num_heads)]) for x in range(scale.shape[0])]).view(B, self.num_heads, T, T)
+
+        weight = weight * scale
+
+        # if mask is not None:
+        #     for b in range(mask.shape[0]):
+        #         for n in range(mask.shape[1]):
+        #             if mask[b][n] is False:
+        #                 weight[b, :, n, :] = float('-inf')
+        #                 weight[b, :, :, n] = float('-inf')
+
+        # TODO for decoder weight must be a triangle matrix
+        weight = F.softmax(weight, dim=-1)
+
+        v = self.value(nodes).view(B, T, self.num_heads, C // self.num_heads).transpose(1, 2)
+
+        res = torch.matmul(weight, v)
+
+        res = res.transpose(1, 2).contiguous().view(B, T, C)
+
+        return self.proj(res)
+
+
+class MCGraphAttention(nn.Module):
+    def __init__(self, in_size, heads=6):
+        super().__init__()
+
+        # self.heads = nn.ModuleList([MCAttentionHead(in_size, heads) for _ in range(heads)])
+        self.heads = MCAttentionHead(in_size, heads)
+
+    def forward(self, nodes, edge_index, mask=None):
+        # return torch.cat([h(nodes, edge_index, mask) for h in self.heads], dim=-1)
+        return self.heads(nodes, edge_index, mask)
+
+# class MCGraphAttention(gnn.conv.MessagePassing):
+#     def __init__(self, emb_size, hidden_size, num_heads):
+#         super().__init__()
+#
+#         self.lin_key = nn.Linear(emb_size, num_heads * emb_size)
+#         self.lin_query = nn.Linear(emb_size, num_heads * emb_size)
+#         self.lin_value = nn.Linear(emb_size, num_heads * emb_size)
+#
+#         self.lin_skip = nn.Linear(emb_size, num_heads * emb_size)
+#
+#         self.num_heads = num_heads
+#         self.emb_size = emb_size
+#
+#     def forward(self, input_tensor, edge_index, mask):
+#         H, C = self.num_heads, self.emb_size
+#
+#         query = self.lin_query(input_tensor[1]).view(-1, H, C)
+#         key = self.lin_key(input_tensor[0]).view(-1, H, C)
+#         value = self.lin_value(input_tensor[0]).view(-1, H, C)
+#
+#         out = self.propagate(edge_index, query=query, key=key, value=value, size=None)
+#
+#         return out.view(-1, H * C)
 
 
 class MCGraphEncoder(Module):
     def __init__(self, emb_size, out_size, num_heads=4, dropout=0.1):
         super().__init__()
 
-        self.attention = MCGraphAttention(emb_size, out_size, num_heads)
+        self.norm1 = gnn.LayerNorm(emb_size)
+        self.attention = MCGraphAttention(emb_size, heads=8)
+        self.norm2 = gnn.LayerNorm(emb_size)
 
         self.feed_forward = nn.Sequential(
-            nn.Linear(emb_size, out_size),
-            nn.Dropout(dropout),
+            nn.Linear(emb_size, 4 * emb_size),
             nn.GELU(),
-            nn.Linear(out_size, emb_size),
+            nn.Linear(4 * emb_size, emb_size),
             nn.Dropout(dropout)
         )
 
-        self.norm = nn.LayerNorm(emb_size)
+        # self.attention = MCGraphAttention(emb_size, out_size, num_heads)
+        #
+        # self.feed_forward = nn.Sequential(
+        #     nn.Linear(emb_size, out_size),
+        #     nn.Dropout(dropout),
+        #     nn.GELU(),
+        #     nn.Linear(out_size, emb_size),
+        #     nn.Dropout(dropout)
+        # )
+        #
+        # self.norm = nn.LayerNorm(emb_size)
 
-    def forward(self, nodes, edge_index):
-        dense_context = self.attention(nodes, edge_index)
+    def forward(self, nodes, edge_index, mask):
+        nodes = self.norm1(nodes)
+        dense_context = self.attention(nodes, edge_index, mask)
 
         res = self.feed_forward(dense_context)
 
-        return self.norm(res)
+        return self.norm2(res)
 
 
 class ThroughputEstimator(pl.LightningModule):
-    def __init__(self, num_opcodes, emb_size, batch_size, hidden_size=256, num_heads=4, dropout=0.1, learning_rate=0.01):
+    def __init__(self, config):
         super().__init__()
+
+        if 'mode' not in config:
+            config['mode'] = 'regression'
+        if 'hidden_size' not in config:
+            config['hidden_size'] = 64
+        if 'dropout' not in config:
+            config['dropout'] = 0.1
+        if 'learning_rate' not in config:
+            config['learning_rate'] = 1e-5
+        if 'num_heads_encoder' not in config:
+            config['num_heads_encoder'] = 4
+        if 'num_heads_decoder' not in config:
+            config['num_heads_decoder'] = 4
+        if 'num_encoders' not in config:
+            config['num_encoders'] = 8
+
+        if config['mode'] != 'pretrain' and config['mode'] != 'regression':
+            raise Exception("Configuration exception")
+
+        self.config = config
+
+        num_opcodes = config['num_opcodes']
+        emb_size = config['embedding_size']
+        num_heads_encoder = config['num_heads_encoder']
+        num_encoders = config['num_encoders']
+        hidden_size = config['hidden_size']
+        dropout = config['dropout']
+
         self.embedding = MCEmbedding(num_opcodes, emb_size)
 
-        self.encoders = nn.ModuleList([MCGraphEncoder(emb_size, hidden_size, num_heads, dropout) for _ in range(4)])
+        self.encoders = nn.ModuleList(
+           [MCGraphEncoder(emb_size, hidden_size, num_heads_encoder, dropout) for _ in range(num_encoders)])
 
         self.token_prediction = nn.Linear(emb_size, num_opcodes)
-        self.softmax = nn.LogSoftmax(dim=-1)
 
-        self.regression = nn.Linear(emb_size, 1)
+        self.proj = nn.Sequential(
+            nn.Linear(emb_size, 4 * emb_size),
+            nn.Linear(4 * emb_size, emb_size)
+        )
+
+        self.regression = nn.Sequential(
+           nn.Linear(emb_size, 1)
+        )
 
         self.train_mae = torchmetrics.MeanAbsoluteError()
         self.val_mae = torchmetrics.MeanAbsoluteError()
 
-        self.lr = learning_rate
-        self.batch_size = batch_size
-        self.num_opcodes = num_opcodes
-
     def forward(self, nodes, edge_index, batch):
-        embedded = self.embedding(nodes)
+        embedded, pos_enc = self.embedding(nodes)
 
-        encoded, _ = to_dense_batch(embedded, batch)
+        encoded, mask = to_dense_batch(embedded, batch)
 
+        # TODO this does not do what I expected
         dense_edges = to_dense_adj(edge_index, batch)
+        dense_edges = dense_edges.view(encoded.shape[0], encoded.shape[1], encoded.shape[1])
 
         for encoder in self.encoders:
-            encoded = encoder(encoded, dense_edges)
+            encoded = encoder(encoded, dense_edges, mask)
 
         token_predictions = self.token_prediction(encoded)
 
-        first_token = encoded[:, 0, :]
+        encoded = self.proj(encoded)
 
-        return self.softmax(token_predictions), self.regression(first_token)
+        x = self.regression(encoded[:, 0, :])
 
-    def training_step(self, batch, batch_idx):
+        return token_predictions, x
+
+    def _step(self, batch, stage: str):
+        assert (stage == 'train') or (stage == 'val')
+
         bb, raw, mask_id, original_token = batch
 
         masked_token, y_hat = self.forward(bb.x, bb.edge_index, bb.batch)
 
-        dense_x, _ = to_dense_batch(bb.x, bb.batch)
+        log_prefix = "train" if stage == 'train' else "val"
 
-        target_token = torch.zeros(masked_token.shape)
+        if self.config['mode'] == 'pretrain':
 
-        for i in range(masked_token.shape[0]):
-            for j in range(masked_token.shape[1]):
-                target_token[i, j, dense_x[i, j]] = 1
+            dense_x, _ = to_dense_batch(bb.x, bb.batch)
 
-        for i in range(self.batch_size):
-            if mask_id[i] != 0:
-                target_token[i, mask_id[i], original_token[i]] = 1
+            target_token = dense_x.clone()
 
-        ml_loss = F.cross_entropy(masked_token, target_token)
+            for i in range(self.config['batch_size']):
+                if mask_id[i] != 0:
+                    target_token[i, mask_id[i]] = original_token[i]
 
-        y_hat = y_hat.reshape(bb.y.shape)
+            loss = F.cross_entropy(masked_token.view(-1, self.config['num_opcodes']), target_token.view(-1).long())
+        else:
+            y_hat = y_hat.reshape(bb.y.shape)
 
-        reg_loss = F.mse_loss(y_hat, bb.y)
+            loss = F.huber_loss(y_hat, bb.y)
 
-        loss = reg_loss + ml_loss
+            mse = F.mse_loss(y_hat, bb.y)
 
-        self.train_mae(y_hat, bb.y)
+            mae_metric = self.train_mae if stage == 'train' else self.val_mae
+            mae_metric(y_hat, bb.y)
 
-        self.log("train_loss", loss, on_epoch=True, batch_size=self.batch_size)
-        self.log("train_ml_loss", ml_loss, on_epoch=True, batch_size=self.batch_size)
-        self.log("train_reg_loss", reg_loss, on_epoch=True, batch_size=self.batch_size)
-        self.log("train_mae", self.train_mae, on_epoch=True, batch_size=self.batch_size)
+            self.log(f"{log_prefix}_mae", mae_metric, on_epoch=True, batch_size=self.config['batch_size'])
+            self.log(f"{log_prefix}_mse", mse, on_epoch=True, batch_size=self.config['batch_size'])
 
+        self.log(f"{log_prefix}_loss", loss, on_epoch=True, batch_size=self.config['batch_size'])
+
+        return loss, y_hat, bb, raw
+
+    def training_step(self, batch, batch_idx):
+        loss, y_hat, bb, raw = self._step(batch, 'train')
         return loss
 
     def validation_step(self, batch, batch_idx):
-        bb, raw, mask_id, original_token = batch
-
-        masked_token, y_hat = self.forward(bb.x, bb.edge_index, bb.batch)
-
-        dense_x, _ = to_dense_batch(bb.x, bb.batch)
-
-        target_token = torch.zeros(masked_token.shape)
-
-        for i in range(masked_token.shape[0]):
-            for j in range(masked_token.shape[1]):
-                target_token[i, j, dense_x[i, j]] = 1
-
-        for i in range(self.batch_size):
-            if mask_id[i] != 0:
-                target_token[i, mask_id[i], original_token[i]] = 1
-
-        ml_loss = F.cross_entropy(masked_token, target_token)
-
-        y_hat = y_hat.reshape(bb.y.shape)
-
-        reg_loss = F.mse_loss(y_hat, bb.y)
-
-        loss = reg_loss + ml_loss
-
-        self.val_mae(y_hat, bb.y)
-
-        self.log("val_loss", loss, on_epoch=True, batch_size=self.batch_size)
-        self.log("val_mae", self.val_mae, on_epoch=True, batch_size=self.batch_size)
+        loss, y_hat, bb, raw = self._step(batch, 'val')
 
         if batch_idx == 0:
             num_samples_to_log = 5
@@ -173,13 +287,38 @@ class ThroughputEstimator(pl.LightningModule):
                     self.logger.experiment.add_text(f"val/sample_{i}/source", raw['source'][i], self.global_step)
 
     def configure_optimizers(self):
-        optimizer = Adam(self.parameters(), lr=self.lr)
-        return optimizer
-        # scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[1, 7, 10, 15, 25, 30], gamma=0.1, verbose=False)
-        # return {
-        #     'optimizer': optimizer,
-        #     'lr_scheduler': {
-        #         'scheduler': scheduler,
-        #         'monitor': 'val_loss',
-        #     }
-        # }
+        optimizer = Adam(self.parameters(), lr=self.config['learning_rate'], weight_decay=1e-3)
+        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, factor=0.1)
+        return {
+            'optimizer': optimizer,
+            'lr_scheduler': {
+                'scheduler': scheduler,
+                'monitor': 'val_loss',
+            }
+        }
+
+    def on_train_start(self):
+        self.logger.log_hyperparams(self.config)
+
+        if self.config['mode'] == 'pretrain':
+            self.regression.requires_grad_(False)
+        else:
+            self.token_prediction.requires_grad_(False)
+            self.embedding.requires_grad_(False)
+            self.encoders.requires_grad_(False)
+    #
+    # def on_validation_epoch_end(self):
+    #     if not self.debug:
+    #         # x = np.asarray(self.val_measurements[0])
+    #         # y = np.asarray(self.val_measurements[1])
+    #         # if np.any(x) and np.any(y):
+    #         #    plot = plot_histogram(x, y, percentile=0.95)
+    #         #    image = PIL.Image.open(plot)
+    #         #    image = ToTensor()(image).unsqueeze(0)
+    #         #    self.logger.experiment.add_image("val_histogram", image[0], self.current_epoch)
+    #
+    #         lift_chart = plot_lift_chart(self.val_measurements)
+    #         image = PIL.Image.open(lift_chart)
+    #         image = ToTensor()(image).unsqueeze(0)
+    #         self.logger.experiment.add_image("val_lift_chart", image[0], self.current_epoch)
+    #     self.val_measurements = [[], []]
